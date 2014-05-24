2991347d377b4cfa853072c9aa3d23b9a9c322bf
littleandroide <jfragoso.perez@gmail.com>
2014-05-24 03:32:02 +0200

    edited quicksort.py

--- algorithm/python/SortAlgorithms/QuickSort.py
+++ algorithm/python/SortAlgorithms/QuickSort.py
@@ -1,6 +1,5 @@
-from random import randrange
-from numpy.matlib import rand
 from numpy.oldnumeric.random_array import randint
+
 
 # Choosing a random pivot minimizes the chance that 
 # you will encounter worst-case O(n2) performance (always 
@@ -8,8 +7,8 @@ from numpy.oldnumeric.random_array import randint
 # for nearly-sorted or nearly-reverse-sorted data). Choosing 
 # the middle element would also be acceptable in the majority of cases.
 
-
 # Quicksort is usually faster than other O(nlogn) algorithms
+
 # best case --> O(nlogn) comparisons
 # average --> O(nlogn) comparisons
 # worst case --> O(n^2)
@@ -34,12 +33,18 @@ def quicksort(numbersList):
     else:
         return numbersList  
 
+def main():
+    runSample()
+    
+    
+def runSample():    
+    numbers = [3, 30, 1, 2, 43, 432 , 43214, 44, 11, 13, 22, 22, 22, 22, 17, 34]
+    
+    print "Initial list" 
+    print str(numbers) 
+    print quicksort(numbers)
 
-numbers = [3, 30, 1, 2, 43, 432 , 43214, 44, 11, 13, 22, 22, 22, 22, 17, 34]
-
-print "Initial list" 
-print str(numbers) 
-print quicksort(numbers)
-
+if __name__ == "__main__":
+    main()
         
         
