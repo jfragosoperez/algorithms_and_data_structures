f4e43c9a6437f8f6444fd12ddbfe0a692775354e
littleandroide <jfragoso.perez@gmail.com>
2014-06-08 21:52:05 +0200

    merge sort + test

--- algorithm/java/src/com/jfragoso/sortalgorithms/MergeSort.java
+++ algorithm/java/src/com/jfragoso/sortalgorithms/MergeSort.java
@@ -7,6 +7,42 @@ public final class MergeSort implements SortAlgorithm{
 
     @Override
     public int[] sortIntegerArray(int[] elements) {
+        if (elements.length <= 1) {
+            return elements;
+        }
+
+        int[] firstPart = new int[elements.length / 2];
+        int[] secondPart = new int[elements.length - firstPart.length];
+        System.arraycopy(elements, 0, firstPart, 0, firstPart.length);
+        System.arraycopy(elements, firstPart.length, secondPart, 0, secondPart.length);
+
+        sortIntegerArray(firstPart);
+        sortIntegerArray(secondPart);
+
+        merge(firstPart, secondPart, elements);
         return elements;
     }
+
+    private void merge(int[] first, int[] second, int [] result) {
+        int elementPositionOfFirstArray = 0;
+        int elementPositionOfSecondArray = 0;
+
+        // Next open position in the result
+        int j = 0;
+
+        while (elementPositionOfFirstArray < first.length && elementPositionOfSecondArray < second.length) {
+            if (first[elementPositionOfFirstArray] < second[elementPositionOfSecondArray]) {
+                result[j] = first[elementPositionOfFirstArray];
+                elementPositionOfFirstArray++;
+            } else {
+                result[j] = second[elementPositionOfSecondArray];
+                elementPositionOfSecondArray++;
+            }
+            j++;
+        }
+
+        System.arraycopy(first, elementPositionOfFirstArray, result, j, first.length - elementPositionOfFirstArray);
+        System.arraycopy(second, elementPositionOfSecondArray, result, j, second.length - elementPositionOfSecondArray);
+    }
+
 }
--- algorithm/java/tests/com/jfragoso/sortalgorithms/SortAlgorithmsFactoryTest.java
+++ algorithm/java/tests/com/jfragoso/sortalgorithms/SortAlgorithmsFactoryTest.java
@@ -1,20 +1,12 @@
 package com.jfragoso.sortalgorithms;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static com.jfragoso.sortalgorithms.SortAlgorithmsFactory.SortAlgorithmType;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class SortAlgorithmsFactoryTest {
-
-    private int[] integerArrayInput;
-    private int[] expectedOutput = new int[]{1, 2, 3, 22, 30, 39, 39, 40};
-
-    @Before
-    public void setUp() throws Exception {
-        integerArrayInput = new int[]{30, 22, 39, 40, 2, 1, 3, 39};
-    }
 
     @Test(expected = NullPointerException.class)
     public void testExceptionIsThrown_ifUndefinedSortAlgorithmTypeIsRequested(){
