210ee3a29cc50ec876326fbceedfc4c5852e3ffc
littleandroide <jfragoso.perez@gmail.com>
2014-06-08 21:34:17 +0200

    generic not needed in the factory

--- algorithm/java/src/com/jfragoso/sortalgorithms/SortAlgorithmsFactory.java
+++ algorithm/java/src/com/jfragoso/sortalgorithms/SortAlgorithmsFactory.java
@@ -16,19 +16,18 @@ public final class SortAlgorithmsFactory {
     /**
      * Retrieves a new instance of the algorithm requested.
      * @param type is the type of the sort algorithm that we want to retrieve
-     * @param <T> is the type of the sort algorithm.
      * @return returns a new instance of the algorithm requested.
      */
-    public static <T extends SortAlgorithm>T create(SortAlgorithmType type){
+    public static SortAlgorithm create(SortAlgorithmType type){
         if (type == null) {
             throw new NullPointerException("You must select the type of algorithm you want to apply.");
         }
         if (type.equals(SortAlgorithmType.INSERTION_SORT)) {
-            return (T) new InsertionSort();
+            return new InsertionSort();
         } else if (type.equals(SortAlgorithmType.QUICK_SORT)) {
-            return (T) new QuickSort();
+            return new QuickSort();
         } else {
-            return (T) new MergeSort();
+            return new MergeSort();
         }
     }
 }
